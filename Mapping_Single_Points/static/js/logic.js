c97da6c2311484c2ac5abcc8ff196b4d90ca9619
sokim03 <sokim03@gmail.com>
2022-04-03 03:07:13 -0700

    adding Simple_Map files

---
+++ Mapping_Single_Points/static/js/logic.js
@@ -0,0 +1,6 @@
+//Code from Leaflet Quick Start Guide
+//var marker = L.marker([51.5, -0.09]).addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
